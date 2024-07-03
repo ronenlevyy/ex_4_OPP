c80cd5d44ac16381d90eead88c8b8b651882604e
ronenlevy <ronenlevy133@gmail.com>
2024-07-03 18:55:39 +0300

    ronen0.1

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,4 +1,64 @@
 package pepse;
 
-public class PepseGameManager {
+import danogl.GameManager; // ודא שיש לך את ההפניה הנכונה ל-GameManager
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import danogl.util.Vector2;
+import pepse.world.Block;
+import pepse.world.Sky;
+import pepse.world.Terrain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PepseGameManager extends GameManager {
+    ImageReader imageReader;
+    SoundReader soundReader;
+    UserInputListener inputListener;
+    WindowController windowController;
+
+
+
+    private void initializeSky(){
+        Sky sky = new Sky();
+        this.gameObjects().addGameObject(sky.create(windowController.getWindowDimensions()),
+                Layer.BACKGROUND);
+    }
+
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener
+            inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        this.imageReader = imageReader;
+        this.soundReader = soundReader;
+        this.inputListener = inputListener;
+        this.windowController = windowController;
+        initializeSky();
+        Terrain tet = new Terrain(windowController.getWindowDimensions(),0);
+        List<Block> blocklist = tet.createInRange(50,700);
+        for(Block block : blocklist){
+            gameObjects().addGameObject(block);
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+
+
 }
---
+++ src/pepse/world/Terrain.java
@@ -0,0 +1,68 @@
+package pepse.world;
+
+import danogl.collisions.GameObjectCollection;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Terrain {
+    private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
+    private Vector2 windowDimensions;
+    private float groundHeightAtX0 ;
+
+
+    //todo- seed isnt important until the unlimited part of the game
+    public Terrain(Vector2 windowDimensions, int seed){
+        this.windowDimensions = windowDimensions;
+        this.groundHeightAtX0 = windowDimensions.y() *  (float) 2/3;
+    }
+
+
+
+    ////////todo- the func isnt ready- we need to implement perline noise
+    ////////todo- the func isnt ready- we need to implement perline noise
+    ////////todo- the func isnt ready- we need to implement perline noise
+    ////////todo- the func isnt ready- we need to implement perline noise
+    public float groundHeightAt(float x) {
+        return groundHeightAtX0;
+    }
+
+    private Block createABlock(Vector2 coordinate){
+        Renderable rectangleRenderable = new RectangleRenderable(ColorSupplier.approximateColor(BASE_GROUND_COLOR));
+        Block block = new Block(coordinate, rectangleRenderable);
+        block.setTag("ground");
+        return block;
+    }
+
+
+
+
+
+
+
+
+    ///todo- keep working on this 2 funcs
+    private float calculateMinX(int initialX){
+        float supply = (float) Math.floor( initialX/ (double) Block.SIZE);
+        return supply*Block.SIZE;
+    }
+    public List<Block> createInRange(int minX, int maxX) {
+        List<Block> blocksList = new ArrayList<>();
+        float newMinX = calculateMinX(minX);
+        float startingY = windowDimensions.y()-Block.SIZE;
+        for (float x = newMinX ; x <= maxX ; x+=Block.SIZE){
+            float yCoord = groundHeightAt(x);
+            for (float y = startingY ; yCoord <= y; y-=Block.SIZE){
+                Block block = createABlock(new Vector2(x, y));
+                blocksList.add(block);
+            }
+        }
+        return blocksList;
+    }
+
+}
